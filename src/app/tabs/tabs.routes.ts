25746896f86a606e0088b7e9847eff4612660131
RolD <rolmandario2015@gmail.com>
2025-11-21 14:39:44 -0500

    Agregacion de navegacion

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -1,30 +1,30 @@
 import { Routes } from '@angular/router';
-import { TabsPage } from './tabs/tabs.page';
+import { TabsPage } from '../tabs/tabs.page';
 
 export const routes: Routes = [
   {
     path: '',
-    component: TabsPage,
+    component: TabsPage, 
     children: [
       {
         path: 'home',
         loadComponent: () =>
-          import('./home/home.page').then(m => m.Homepage),
+          import('../home/home.page').then(m => m.Homepage),
       },
       {
         path: 'explorar',
         loadComponent: () =>
-          import('./pages/explorar/explorar.page').then(m => m.ExplorarPage),
+          import('../pages/explorar/explorar.page').then(m => m.ExplorarPage),
       },
       {
         path: 'favoritos',
         loadComponent: () =>
-          import('./pages/favoritos/favoritos.page').then(m => m.FavoritosPage),
+          import('../pages/favoritos/favoritos.page').then(m => m.FavoritosPage),
       },
       {
         path: 'ajustes',
         loadComponent: () =>
-          import('./pages/ajustes/ajustes.page').then(m => m.AjustesPage),
+          import('../pages/ajustes/ajustes.page').then(m => m.AjustesPage),
       },
       {
         path: '',
