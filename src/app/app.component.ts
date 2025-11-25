4e8b80fa41c406f372de24ed660f1e135084bf6a
RolD <rolmandario2015@gmail.com>
2025-11-24 21:00:36 -0500

    Bugs Fixed V3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from './services/storage.service';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
@@ -12,5 +13,11 @@ export class AppComponent implements OnInit {
 
   async ngOnInit() {
     await this.storageService.init();
+
+    // Configuración de la barra de estado
+    StatusBar.setOverlaysWebView({ overlay: false }); // Evita que el contenido se meta debajo
+    StatusBar.setStyle({ style: Style.Light });       // Cambia el estilo (Light/Dark)
+    StatusBar.setBackgroundColor({ color: '#ffffff' }); // Color de fondo de la barra
   }
 }
+
